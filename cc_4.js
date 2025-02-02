f4f18a4d502845fa48a0a4ac422c941aab73435b
Alikhan <fixator2004@gmail.com>
2025-02-02 17:43:03 -0500

    Task 1 - Customer Discounts.

---
+++ cc_4.js
@@ -0,0 +1,14 @@
+//Task 1: If Statements
+
+let purchaseAmount = 300; //declared a variable with a value
+let final_amount = purchaseAmount * 0.9; //10% discount is applied if purchase amount is > $100
+
+if (purchaseAmount > 100) {
+    console.log(`Final amount after discount: $${final_amount}`); //the code loggs the final amount with discount applied if purchase amount > $100
+
+}else{
+    console.log(`Final amount after discount: $${purchaseAmount}`); //the code loggs the initial amount without a discount if purchase amount < $100
+    };
+
+
+
